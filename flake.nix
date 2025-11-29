81a4c49291324c7f4613020fa233926883fc534f
imlyzh <enterindex@gmail.com>
2025-11-29 21:50:23 +0800

    feat: update flake inputs to 25.11 release channels
    Update nixpkgs, nix-darwin, and home-manager inputs to use the latest 25.11 release channels. This includes:
    - Switching nixpkgs from 25.05-darwin to 25.11-darwin
    - Updating nix-darwin to latest main branch (removing 25.05 ref)
    - Updating home-manager to latest main branch (removing 25.05 ref)
    
    The changes ensure compatibility with the latest package versions and features available in the 25.11 release cycle.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,15 @@
   description = "Lyzh's Nix-Darwin configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
 
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    # nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    # home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     home-config.url = "github:imlyzh/home-manager";
